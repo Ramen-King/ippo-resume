8372fd4c8e5cb116c560e09f6c8a57770c2b20b6
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-05-04 19:09:27 -0400

    added button event liseners in init

--- main.js
+++ main.js
@@ -1,45 +1,65 @@
 window.onload = init;
-//try 
+
 function init(){
-    hideContainer();
-    setTimeout(activateOverlay, 2500);
+    displayMenu();
+    let buttons = document.getElementsByClassName('nav-item');
+    const allButtons = Array.from(buttons);
+    allButtons.forEach(value => value.addEventListener('click', loadingAnimation))
+ 
+    console.log(Array.from(buttons));
+   
     
-    
+     
 }
 function animate(event){
     const section = event.target;
     section.style.animate = '1s'
 }
+function buttons(event){
+event.preventDefault();
 
-function activateOverlay(){
-    const container = document.getElementById('display-menu');
-    if (container.style.display === "none") {
-        console.log('hi')
+}
+function displayMenu(){
+    const container = document.querySelector('#display-menu');
+    container.style.display = "block";
+    console.log(container.style.display)
+    //container.( item => {item.addEventListener('click', loadingAnimation)})
+
+    //container.forEach(navItem => navItem.addEventListener('click', loadingAnimation));
+     if (container.style.display === "none") {
         container.style.display = "block";
-    } else {
-        container.style.display = "none";
-    }
 }
-function hideContainer(){
-    const container = document.getElementById('display-menu');
-    container.style.display = "none";
 }
-function handleMenu(event){
-    event.preventDefault();
-    setTimeout(hideContainer, 100)
-    console.log('pewpew')
-    document.querySelector('img').src = "openingClickNL.gif";
-    displayContent();
-}
+
 function displayContent(){
     const container = document.getElementById('display-content');
-    const createLi = document.createElement('p');
-        createLi.innerText = aboutMe.bio;
-        container.appendChild(createLi);
     if (container.style.display === "none") {
-        console.log('hi')
         container.style.display = "block";
     } else {
         container.style.display = "none";
     }
+
 }
+
+
+
+
+//reusables
+function backButton(event){
+    event.preventDefault();
+    document.getElementById('back-button').addEventListener('click', displayMenu)
+}
+
+
+function loadingAnimation(){
+    document.querySelector('img').src = "openingClickNL.gif";
+
+    
+}
+//img tag will take care of all backgrounds
+//"main-container" will be the main canvas;
+//set a timeout to allow animation to run to the last frame.
+//invoke display-menu to change display value
+//for each section, add a eventlistener to change display value & .src value
+//set timeout to again for when the loading animation finishes ** maybe i should make all the gifs the same length...**
+//invoke display-content to change display value
